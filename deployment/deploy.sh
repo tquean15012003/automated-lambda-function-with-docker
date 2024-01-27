d539cf52d99f1a0ff637bac06a665ae2bd674276
tquean15012003 <tquean15012003@gmail.com>
2024-01-27 11:02:45 +0800

    build(deployment): change application name

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -7,10 +7,10 @@ fi
 
 # Step 2: Specify your aws credentials and app name
 aws_profile="AdministratorAccess-007985056474"
-app="lambda-docker"
+app="lambda-presidio"
 aws_account="007985056474"
 aws_region="ap-southeast-1"
-version="0.0.2"
+version="0.0.1"
 
 # Step 3: Run uploading image script
 source ./upload_image.sh $aws_profile $aws_region $env $app $aws_account $version
